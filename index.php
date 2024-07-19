6540255dcfa162179f0f8ebf2cef256087366b94
Rizky Rahman <rizk87devops@gmail.com>
2024-07-19 21:54:52 +0800

    list

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "Hello world";
+    // echo "Hello world";
+    // Variabel dan Tipe data
+    $nama_siswa = "Budi"; // String
+    $usia_siswa = 20; // Integer
+    $status_kelulusan = false;
+    $list_siswa = ["Budi", "Andi", "Rudi"]
+
+    echo $list_siswa;
+
 ?>
