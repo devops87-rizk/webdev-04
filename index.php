3c797ffac6ad113c41ed5c42b01cb4ffc8e7cd17
Rizky Rahman <rizk87devops@gmail.com>
2024-07-19 22:03:43 +0800

    aritmatika

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
     // echo "Hello world";
     // Variabel dan Tipe data
-    $nama_siswa = "Budi"; // String
-    $usia_siswa = 20; // Integer
+    $nama_siswa = "Jan";
+    $usia_siswa = 20;
     $status_kelulusan = false;
-    $list_siswa = ["Budi", "Andi", "Rudi"]
+    $list_siswa = ["Budi", "Andi", "Rudi"];
 
-    echo $list_siswa;
+    $angka_a = 30;
+    $angka_b = 40;
+    
+
+    echo $angka_a + $angka_b;
 
 ?>
