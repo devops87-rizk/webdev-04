3849eb77b0831d4f9daceafd1e3b14546f06d923
Rizky Rahman <rizkpython87@gmail.com>
2024-07-27 20:59:10 +0800

    change form

--- form.php
+++ form.php
@@ -9,14 +9,16 @@
     <h1>Form Pendaftaran</h1>
 
     <form action="tujuan.php" method="get">
+
         <label for="">Nama Lengkap</label>
         <input type="text" name="nama_lengkap">
+
         <br>
+
         <label for="">Usia</label>
-        <input type="text" age="usia">
-        <br>
-        <label for="">Jenis Kelamin</label>
-        <input type="text" sex="jenis_kelamin">
+        <input type="text" name="usia">
+        
+        <button type="submit">Kirim Data</button>
     </form>
 </body>
 </html>
---
+++ tujuan.php
@@ -0,0 +1,7 @@
+<?php
+    $nama_dari_form = $_GET["nama_lengkap"];
+    $usia_dari_form = $_GET["usia"];
+
+    echo "Hallo" . $nama_dari_form . ". Saat ini anda berusia" . $usia_dari_form . ". Selamat data di web saya";
+    
+?>
