ddd6ca054cb6f9a9619d54cf74c777ac65b8a293
Rizky Rahman <rizkpython87@gmail.com>
2024-07-27 21:01:43 +0800

    spasi

--- tujuan.php
+++ tujuan.php
@@ -2,6 +2,6 @@
     $nama_dari_form = $_GET["nama_lengkap"];
     $usia_dari_form = $_GET["usia"];
 
-    echo "Hallo" . $nama_dari_form . ". Saat ini anda berusia" . $usia_dari_form . ". Selamat data di web saya";
-    
+    echo "Hallo " . $nama_dari_form .  ". Saat ini anda berusia " . $usia_dari_form . ". Selamat data di web saya";
+
 ?>
