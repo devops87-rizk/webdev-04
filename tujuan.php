83ef9b6e5437d74c76b51c4889f2c7fba8052d4c
Rizky Rahman <rizkpython87@gmail.com>
2024-07-28 14:59:35 +0800

    missing

--- tujuan.php
+++ tujuan.php
@@ -1,8 +1,3 @@
 <?php
-    $nama_dari_form = $_POST["nama_lengkap"];
-    $usia_dari_form = $_POST["usia"];
-
-    echo "Hallo " . $nama_dari_form .  ". Saat ini anda berusia " . $usia_dari_form . ". Selamat data di web saya";
-
-
+    echo $_POST['nama_lengkap']
 ?>
